0fb5c605a25c3e1b53cf45448e6eca2d8ddf58de
kraken1970 <kraken@hi.ru>
2018-02-01 19:50:26 +0300

    1-st commit

---
+++ src/assets/JS/price.js
@@ -0,0 +1,23 @@
+
+let linkSearchMap2 = document.querySelector(".search-map2");
+let map = document.querySelector(".modal-content-map");
+let closeMap = map.querySelector(".modal-content-close");
+
+
+linkSearchMap2.addEventListener("click", (event) => {
+  event.preventDefault();
+  map.classList.add("modal-map-show");
+});
+
+closeMap.addEventListener("click", (event) => {
+  event.preventDefault();
+  map.classList.remove("modal-map-show");
+});
+
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode === 27) {
+    if (map.classList.contains("modal-map-show")) {
+    map.classList.remove("modal-map-show");
+    }
+  }
+});
